c1fc62843b6df213618ceccb911a691d70975508
banderhlog <grahpon@gmail.com>
2021-02-16 00:04:46 +0700

    Добавлена функция получения соседей

--- type.py
+++ type.py
@@ -1,17 +1,31 @@
 import pandas as pd
 
-centers = pd.read_csv('/Users/sanek/Desktop/NSU/cell_3d_model/Centroid_correct.csv')
-centers = centers[:-1]
-#cell_walls = pd.read_excel('/Users/sanek/Desktop/NSU/cell_3d_model/cellwall_length_correct.xls')
-
+centers = pd.read_csv('/Users/sanek/Desktop/NSU/cell_3d_model/Centroid_correct.csv', header= None)
+cell_walls = pd.read_csv('/Users/sanek/Desktop/NSU/cell_3d_model/cellwall_length_correct.csv', sep=';', header= None)
+print(centers.tail())
+print(cell_walls.head())
 x, y = [], []
 #считывание таблицы координат центров
 for i in centers.iterrows():
-    x_y = list(map(float,i[1]['x;y'].split(';')))
+    x_y = list(map(float,i[1][0].split(';')))
     x.append(x_y[0])
     y.append(x_y[0])
 centers['x'] = x
 centers['y'] = y
-centers = centers.drop('x;y', axis= 1)
+centers = centers.drop(0, axis= 1)
 del x
 del y
+print(centers.tail())
+
+def neighbours(column):
+    if 0 <= column <= centers.shape[0] - 1:
+        n, m = [], []
+        for i in range(cell_walls.shape[1]):
+            d = cell_walls.loc[column][i]
+            if d != 0 and i != 0 :
+                n.append(i)
+                m.append(d)
+        print(n)
+        print(m)
+        res = dict(zip(n,m))
+        return res
