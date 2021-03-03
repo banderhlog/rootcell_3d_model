623c1e7dc806a9f1952ccddfba99c8d1c358e38e
banderhlog <grahpon@gmail.com>
2021-03-03 16:03:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import cell as c
 import cell_pillar as cp
 import root as rt
 import type
-
-MainRoot = rt.Root(4, list('abcd'))
+import graphics
+types = [1, 2, 3, 4]
+MainRoot = rt.Root(4, types)
 #print(MainRoot.info())
-cells1 = [[0,1],[1,2],[2,2.5]]
-cells2 = [[0,0.5],[0.5, 1.5],[1.5,3]]
-MainRoot.set_pillar_cells(2, cells1)
-MainRoot.set_pillar_cells(1,cells2)
+cells1 = [[0,10]]
+cells2 = [[0,5],[5,10],[10,15]]
+cells3 = [[0,3],[3,10],[10,15]]
+cells4 = [[0,6],[6,10],[10,20]]
+MainRoot.set_pillar_cells(0, cells1)
+MainRoot.set_pillar_cells(1, cells2)
+MainRoot.set_pillar_cells(2, cells3)
+MainRoot.set_pillar_cells(3, cells4)
+print('-----------------------------')
+MainRoot.get_pillars()[2].get_cell(0).info()
 ef = MainRoot.elementary_fragmentation()
-#print(ef)
-ef_to_c = cp.list_to_cell(ef)
-print(ef_to_c)
-MainRoot.set_pillar_cells(0,ef_to_c)
-print(MainRoot.get_pillar(0).get_cell_wh(3))
+print(ef)
+print(MainRoot.get_types())
+MainRoot.metabolism(1)
+#print(MainRoot.get_neighbours(1, 0))
+print(type.center(0))
+graphics.draw_root(MainRoot)
